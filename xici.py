abd5d8627eb615d1bf0185482ca921290e762c22
neilxu <mr.neilxu@gmail.com>
2016-12-02 22:50:52 +0800

    add

--- xici.py
+++ xici.py
@@ -6,15 +6,15 @@ import urllib2
 import requests
 from bs4 import BeautifulSoup
 
-def get_xici_ip_from_api(None):
-        start_url='http://api.xicidaili.com/free2016.txt'
-        headers={
-                'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari'
-        }
-        r=requests.get(start_url,headers=headers)
-        f=open('result/xici.txt','w')
-        f.write(r.text)
-        f.close()
+# def get_xici_ip_from_api(None):
+#         start_url='http://api.xicidaili.com/free2016.txt'
+#         headers={
+#                 'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari'
+#         }
+#         r=requests.get(start_url,headers=headers)
+#         f=open('result/xici.txt','w')
+#         f.write(r.text)
+#         f.close()
 
 
 # 待调试
