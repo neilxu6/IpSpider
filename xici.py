5bf685d39b20367cf40c38a231daccd8fd4ffbe0
neilxu <mr.neilxu@gmail.com>
2016-12-03 15:53:37 +0800

    xici well done

--- xici.py
+++ xici.py
@@ -1,61 +1,84 @@
-#!/usr/bin/env python
-
 # -*- coding: utf-8 -*-
 
+
+from bs4 import BeautifulSoup
 import urllib2
 import requests
-from bs4 import BeautifulSoup
+import time
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
-# def get_xici_ip_from_api(None):
+
+# 来源一：从api中获取ip
+# def get_xici_ip_from_api():
 #         start_url='http://api.xicidaili.com/free2016.txt'
 #         headers={
 #                 'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari'
 #         }
 #         r=requests.get(start_url,headers=headers)
-#         f=open('result/xici.txt','w')
-#         f.write(r.text)
-#         f.close()
+#         f5.write('http://'+str(r.text))
+#         print ('http://'+str(r.text))
 
 
-# 待调试
+# 来源二：从网页上爬取ip
 def get_xici_ip_from_webpages(ip_type):
         countNum = 0
-        proxyFile = open('proxy.txt', 'a')
         start_url='http://www.xicidaili.com/'+str(ip_type)+'/'
 
         requestHeader = {
                 'User-Agent': "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36"}
 
-        for page in range(1, 10):
+        for page in range(1, 20):
                 url = start_url + str(page)
-                # print url
+                time.sleep(2)
                 request = urllib2.Request(url, headers=requestHeader)
                 html_doc = urllib2.urlopen(request).read()
 
                 soup = BeautifulSoup(html_doc, "html.parser")
-                # print soup
                 trs = soup.find('table', id='ip_list').find_all('tr')
                 for tr in trs[1:]:
                         tds = tr.find_all('td')
-                        # 国家
-                        if tds[1].find('img') is None:
-                                nation = '未知'
-                                locate = '未知'
-                        else:
-                                nation = tds[1].find('img')['alt'].strip()
-                                locate = tds[4].text.strip()
-                        ip = tds[2].text.strip()
-                        port = tds[3].text.strip()
-                        anony = tds[5].text.strip()
-                        protocol = tds[6].text.strip()
-                        speed = tds[7].find('div')['title'].strip()
-                        time = tds[9].text.strip()
-
-                        proxyFile.write(
-                                '%s|%s|%s|%s|%s|%s|%s|%s\n' % (nation, ip, port, locate, anony, protocol, speed, time))
-                        # print '%s=%s:%s' % (protocol, ip, port)
+                        protocol = str(tds[5].text.strip())
+                        ip=str(tds[1].text.strip())
+                        port = str(tds[2].text.strip())
+                        # local = str(tds[3].text.strip())
+                        # speed = str(tds[7].find('div')['title'].strip())
+                        # time = str(tds[9].text.strip())
                         countNum += 1
-        proxyFile.close()
+                        print ('http://'+ip+':'+port)
+                        if str(ip_type) in 'nn' or 'nt':
+                                if str(protocol)=='HTTP':
+                                        f1.write('http://'+ip+':'+port+'\n')
+                                else:
+                                        f2.write(protocol.lower()+'://'+ip+':'+port+'\n')
+                        elif str(ip_type) in 'wn' or 'wt':
+                                if str(protocol) == 'HTTP':
+                                        f3.write('http://' + ip + ':' + port + '\n')
+                                else:
+                                        f4.write(protocol.lower() + '://' + ip + ':' + port + '\n')
+                        else:
+                                pass
+        print '共'+countNum+'个ip'
         return countNum
 
-get_xici_ip_from_webpages('nn')
+
+
+if __name__ == '__main__':
+        f0=open('results/xici_results/xici.txt','a')
+        f1=open('results/xici_results/xici_http_china.txt', 'a')
+        f2=open('results/xici_results/xici_other_protocols_china.txt','a')
+        f3=open('results/xici_results/xici_http_aborad.txt','a')
+        f4=open('results/xici_results/xici_other_protocols_aborad.txt','a')
+        f5=open('results/xici_results/xici_api.txt','a')
+
+        get_xici_ip_from_webpages('nn')
+
+        # get_xici_ip_from_api()
+
+        f0.close()
+        f1.close()
+        f2.close()
+        f3.close()
+        f4.close()
+        f5.close()
